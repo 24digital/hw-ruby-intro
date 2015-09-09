4bcd5b8ccbd96ad08e328c6a76990c6b39aaafe0
a24digital <24digital@gmail.com>
2015-09-09 22:41:19 +0000

    test

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,63 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+total = 0
+return 0 if arr.empty?
+ 
+ arr.each do |x|
+   total +=  x
+end
+ total
+
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  total = 0
+  
+return 0 if arr.empty?
+return arr.at(0) if arr.length == 1
+total = arr.sort[-2]
+total += arr.sort[-1]
+return total
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+ !!arr.uniq.combination(2).detect { |a, b| a + b == n }
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  temp = "Hello, "
+temp.concat(name) 
+return temp
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+ return false if s.empty?
+    return true if s[0].match(/b|c|d|f|g|h|j|k|l|m|n|p|q|r|s|t|v|w|y|x|z/)
+    return true if s[0].match(/B|C|D|F|G|H|J|K|L|M|N|P|Q|R|S|T|V|W|Y|X|Z/)
+return false
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+    return false if s.nil?
+  s =~ /^[01]*1[01]*00$/
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_reader :isbn, :price
+  attr_accessor :isbn, :price   
+   
+    def initialize(isbn, price)
+        raise  ArgumentError.new() if isbn.empty? || (price<=0)
+      @isbn = isbn
+@price = Float(price)
+    end
+    
+def price_as_string
+     "$%.2f" % @price
+end
 end
